7e5dc3b9bdf5ee62797604e9b88366a91ee7be91
Craig Condon <craig.j.condon@gmail.com>
2018-12-19 13:28:30 -0600

    initial commit

---
+++ src/entry.ts
@@ -0,0 +1,9 @@
+
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+import { Application } from "./components/main/view.pc";
+
+const mount = document.createElement("div");
+document.body.appendChild(mount);
+
+ReactDOM.render(React.createElement(Application), mount);
